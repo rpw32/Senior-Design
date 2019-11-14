9799cba24e91b0970dc24631a56fe2e6ad017d36
zuphoff <57680341+zuphoff@users.noreply.github.com>
2019-11-13 20:11:17 -0600

    Working search function. Very unpolished, but displays results on screen.

--- app/src/main/java/com/design/senior/MainActivity.java
+++ app/src/main/java/com/design/senior/MainActivity.java
@@ -1,13 +1,28 @@
 package com.design.senior;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openActivitySearch();
+            }
+        });
+    }
+
+    public void openActivitySearch() {
+        Intent intent = new Intent(this, activity_search_result.class);
+        startActivity(intent);
     }
 }
