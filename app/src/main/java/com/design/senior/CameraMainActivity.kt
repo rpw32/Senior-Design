2e53c9582e80eb129e24e31172f3b42eb17b56db
rpw32 <rpw.32@live.com>
2019-11-15 13:16:56 -0600

    Added Kotlin camera function

---
+++ app/src/main/java/com/design/senior/CameraMainActivity.kt
@@ -0,0 +1,12 @@
+package com.design.senior
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_main)
+    }
+}
