bbdab05b990968307c41628efa41f3a8a0183db6
rpw32 <rpw.32@live.com>
2019-11-15 13:36:14 -0600

    Added Kotlin camera function

--- app/src/main/java/com/design/senior/MainActivity.java
+++ app/src/main/java/com/design/senior/MainActivity.java
@@ -34,72 +34,9 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int REQUEST_CODE_PERMISSIONS = 101;
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
-    TextureView textureView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textureView = findViewById(R.id.view_finder);
-
-        if(allPermissionsGranted()){
-            startCamera(); //start camera if permission has been granted by user
-        } else{
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
-        }
-    }
-
-    private void startCamera() {
-        CameraX.unbindAll();
-
-        PreviewConfig config = new PreviewConfig.Builder()
-                .setLensFacing(CameraX.LensFacing.BACK)
-                .build();
-        Preview preview = new Preview(config);
-
-        preview.setOnPreviewOutputUpdateListener(
-                new Preview.OnPreviewOutputUpdateListener() {
-                    @Override
-                    public void onUpdated(Preview.PreviewOutput previewOutput) {
-                        ViewGroup parent = (ViewGroup) textureView.getParent();
-                        parent.removeView(textureView);
-                        parent.addView(textureView, 0);
-
-                        textureView.setSurfaceTexture(previewOutput.getSurfaceTexture());
-
-                        // Compute center of preview
-                        float centerX = (float)textureView.getWidth()/2;
-                        float centerY = (float)textureView.getHeight()/2;
-
-                        int rotationDegrees;
-
-                        switch((int)textureView.getRotation()) {
-                            case Surface.ROTATION_0: rotationDegrees = 0; break;
-                            case Surface.ROTATION_90: rotationDegrees = 90; break;
-                            case Surface.ROTATION_180: rotationDegrees = 180; break;
-                            case Surface.ROTATION_270: rotationDegrees = 270; break;
-                            default: return;
-                        }
-
-                        Matrix matrix = new Matrix();
-                        matrix.postRotate((float)rotationDegrees,centerX,centerY);
-
-                        textureView.setTransform(matrix);
-                    }
-                });
-
-        CameraX.bindToLifecycle(this, preview);
-    }
-
-    private boolean allPermissionsGranted(){
-
-        for(String permission : REQUIRED_PERMISSIONS){
-            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
-                return false;
-            }
-        }
-        return true;
     }
 }
